4a96757727bdf1814ec17b5ada2922277add1990
Bosque Gin <bosquegin@bosquegin.com>
2026-06-29 12:51:06 -0300

    data: actualizar 2026-06-29 12:51

--- data_meta.js
+++ data_meta.js
@@ -1,1 +1,1 @@
-window.BOSQUE_DATA=window.BOSQUE_DATA||{};window.BOSQUE_DATA.meta={"generado": "2026-06-29T12:22:05-03:00", "ventas_hasta": "2026-06", "stock_hasta": "2026-06-26", "destileria_hasta": "?"};
+window.BOSQUE_DATA=window.BOSQUE_DATA||{};window.BOSQUE_DATA.meta={"generado": "2026-06-29T12:51:02-03:00", "ventas_hasta": "2026-06", "stock_hasta": "2026-06-26", "destileria_hasta": "?"};
